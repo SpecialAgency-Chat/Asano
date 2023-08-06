612bf17bde467790e86927f73d88d7ff6097dd8a
waki285 <suzuneu@suzuneu.com>
2023-08-06 12:17:15 +0900

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import interactionsRouter from "@/routes/interactions";
 import invitesRouter from "@/routes/invites";
 import joinrequestRouter from "@/routes/joinrequest";
 import cron from "@/cron";
 
 const app = new Hono();
+
+app.use(
+  "/*",
+  cors({
+    origin: "http://sachat.cloud",
+    allowMethods: ["POST", "GET", "OPTIONS", "HEAD"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
 
 app.get("/", (c) => c.text("Hello World!"));
 
